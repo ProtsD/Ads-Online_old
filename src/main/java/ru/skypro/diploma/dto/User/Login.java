ed0aa0c4af41ecf26f6657c063c58c640c75de20
RUPROTSD <protsdv@gmail.com>
2023-09-24 22:48:22 +0300

    DTOs (Login, NewPassword, Register, UpdateUser, User), UserController have been created or modified.

--- src/main/java/ru/skypro/diploma/dto/User/Login.java
+++ src/main/java/ru/skypro/diploma/dto/User/Login.java
@@ -3,10 +3,13 @@ package ru.skypro.diploma.dto.User;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.Size;
+
 @Component
 @Data
 public class Login {
-
+    @Size(min = 8, max = 16)
     private String username;
+    @Size(min = 4, max = 32)
     private String password;
 }
