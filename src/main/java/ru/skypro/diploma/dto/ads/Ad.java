4467b5c60aa369822ee97d295202ace3387508fe
Baggovut <117596144+Baggovut@users.noreply.github.com>
2023-09-26 22:28:24 +0300

    small fixes

--- src/main/java/ru/skypro/diploma/dto/ads/Ad.java
+++ src/main/java/ru/skypro/diploma/dto/ads/Ad.java
@@ -2,11 +2,18 @@ package ru.skypro.diploma.dto.ads;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
 @Data
 public class Ad {
-    private Integer author;
+    private int author;
     private String image;
-    private Integer pk;
-    private Integer price;
+    private int pk;
+    @Min(0)
+    @Max(10000000)
+    private int price;
+    @Size(min = 4, max = 32)
     private String title;
 }
