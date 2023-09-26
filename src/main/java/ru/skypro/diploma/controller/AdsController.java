6317be72e78ccb49ef51b7878efe45fe8c31c51d
Baggovut <117596144+Baggovut@users.noreply.github.com>
2023-09-27 00:47:48 +0300

    fixes for Ads controller

--- src/main/java/ru/skypro/diploma/controller/AdsController.java
+++ src/main/java/ru/skypro/diploma/controller/AdsController.java
@@ -1,7 +1,6 @@
 package ru.skypro.diploma.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
